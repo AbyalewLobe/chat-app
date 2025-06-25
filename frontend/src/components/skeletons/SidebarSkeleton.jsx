259ad807c56bdaa40524a53635dcbcc080f4e490
AbyalewLobe <labyalew@gmail.com>
2025-06-25 07:24:40 -0900

    fix sidebarSkeleton

--- frontend/src/components/skeletons/SidebarSkeleton.jsx
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -6,7 +6,7 @@ const SidebarSkeleton = () => {
   return (
     <aside
       className="h-full w-20 lg:w-72 border-r border-base-300 
-    flex flex-col transition-all duration-200"
+      flex flex-col transition-all duration-200 bg-base-100"
     >
       {/* Header */}
       <div className="border-b border-base-300 w-full p-5">
@@ -17,18 +17,21 @@ const SidebarSkeleton = () => {
       </div>
 
       {/* Skeleton Contacts */}
-      <div className="overflow-y-auto w-full py-3">
+      <div className="overflow-y-auto w-full py-3 space-y-4">
         {skeletonContacts.map((_, idx) => (
-          <div key={idx} className="w-full p-3 flex items-center gap-3">
+          <div
+            key={idx}
+            className="w-full px-4 flex items-center gap-3 animate-pulse"
+          >
             {/* Avatar skeleton */}
             <div className="relative mx-auto lg:mx-0">
-              <div className="skeleton size-12 rounded-full" />
+              <div className="skeleton size-12 rounded-full bg-base-200" />
             </div>
 
-            {/* User info skeleton - only visible on larger screens */}
-            <div className="hidden lg:block text-left min-w-0 flex-1">
-              <div className="skeleton h-4 w-32 mb-2" />
-              <div className="skeleton h-3 w-16" />
+            {/* User info skeleton */}
+            <div className="hidden lg:block text-left min-w-0 flex-1 space-y-1">
+              <div className="skeleton h-4 w-32 bg-base-200 rounded" />
+              <div className="skeleton h-3 w-16 bg-base-200 rounded" />
             </div>
           </div>
         ))}
