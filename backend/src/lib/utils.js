3f2d261513765c9cf01228efa964d5bd45675226
AbyalewLobe <labyalew@gmail.com>
2025-09-26 00:02:39 -0700

    commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,13 +2,15 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '7d',
+    expiresIn: '2d',
   });
+
   res.cookie('jwt', token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    maxAge: 2 * 24 * 60 * 60 * 1000,
+    httpOnly: true, // prevent xss attack
+    sameSite: 'none',
+    secure: true,
   });
+
   return token;
 };
