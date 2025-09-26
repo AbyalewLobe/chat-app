2fcbbfb46a0dee1589942b7ce3364543349fbb10
AbyalewLobe <labyalew@gmail.com>
2025-09-25 23:38:02 -0700

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,10 +17,14 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://chat-app-five-umber-58.vercel.app',
+    ],
     credentials: true,
   })
 );
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/api/auth', authRoutes);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === 'development'
       ? 'http://localhost:5001/api'
-      : '/api',
+      : 'https://chat-app-6rw1.onrender.com/api',
   withCredentials: true,
 });
 
